c513f8a83db4f80079ebd623d80658c962a5ad30
Armando del Río <adrio.coder@gmail.com>
2021-10-24 20:04:17 -0500

    boostrap personal next ts starter

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+// _app.tsx
+import '@styles/globals.css';
+
+import React, { FC } from 'react';
+import { AppProps } from 'next/app';
+
+const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+export default MyApp;
