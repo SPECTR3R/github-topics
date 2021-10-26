b5c8230d19ef445975fc9d0728b70862c5e8f3fd
Armando del Río <adrio.coder@gmail.com>
2021-10-25 21:40:02 -0500

    resposive desing

--- src/components/TopicsContainer/Topics.tsx
+++ src/components/TopicsContainer/Topics.tsx
@@ -22,8 +22,8 @@ export function Topics({
   stargazerCount: number;
 }) {
   return (
-    <Flex p={{ base: 5, sm: 10, md: 20 }} w="auto" justifyContent="center" alignItems="center">
-      <Box px={{ base: 5, sm: 20, md: 30 }} py={20} mx="auto" bg="white" shadow={{ base: 'none', md: 'xl' }}>
+    <Flex w={{ base: '100%' }}>
+      <Box px={{ base: 5, sm: 20, md: 30 }} w="auto" py={20} mx="auto" bg="white" shadow={{ base: 'none', md: 'xl' }}>
         <Box textAlign="center">
           <Heading />
           <SearchbarContainer />
@@ -32,13 +32,13 @@ export function Topics({
         <Box mt={10} fontSize="xl" color="gray.500">
           {relatedTopics.length > 0 ? 'Related Topics:' : 'No related Topics found. :('}
         </Box>
-        <SimpleGrid columns={{ base: 2, sm: 2, md: 3, lg: 4, xl: 6 }} spacingX={{ base: 16, lg: 24 }} mt={6}>
+        <Flex columns={{ base: 2, sm: 2, md: 3, lg: 4, xl: 6 }} spacingX={{ base: 16, lg: 24 }} mt={6} wrap="wrap">
           {relatedTopics.map(({ name, stargazers, id }) => (
             <Fragment key={id}>
               <TopicCard title={name} count={stargazers.totalCount} key={id} />
             </Fragment>
           ))}
-        </SimpleGrid>
+        </Flex>
       </Box>
     </Flex>
   );
